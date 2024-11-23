ad7a01edbb467a6f23f1a7e71ef7a6d7b38dd7fe
Michael Haynie <michael.dean.haynie@gmail.com>
2024-11-23 16:12:49 -0700

    simplify example

--- src/cli.ts
+++ src/cli.ts
@@ -1,25 +1,29 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import packageJson from '../package.json';
+
 const program = new Command();
 
 program
-  .name('my-cli-tool')
-  .description('A sample CLI tool written in TypeScript')
-  .version('1.0.0');
+  .name('kommand')
+  .alias('kd')
+  .description(packageJson.description)
+  .version(packageJson.version);
 
-program
-  .option('-v, --verbose', 'Enable verbose mode')
-  .option('-c, --config <path>', 'Specify a config file');
 
-program
-  .argument('<positional>', 'A required positional argument')
-  .argument('[optional]', 'An optional positional argument');
+// program
+//   .option('-v, --verbose', 'Enable verbose mode')
+//   .option('-c, --config <path>', 'Specify a config file');
 
-program.action((positional, optional, options) => {
-  console.log('Positional Argument:', positional);
-  console.log('Optional Argument:', optional);
-  console.log('Options:', options);
-});
+// program
+//   .argument('<positional>', 'A required positional argument')
+//   .argument('[optional]', 'An optional positional argument');
+
+// program.action((positional, optional, options) => {
+//   console.log('Positional Argument:', positional);
+//   console.log('Optional Argument:', optional);
+//   console.log('Options:', options);
+// });
 
 program.parse();
